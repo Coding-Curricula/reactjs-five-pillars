0e3a4541316e9fcb98ef06ffd31ff0583349600b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-21 22:51:23 -0500

    added joi

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,41 @@
 import React, { Fragment, useState, useEffect, useCallback } from 'react'
+import Joi from 'joi-browser';
+
+const usersSchema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+    username: Joi.string().required(),
+    email: Joi.string().required(),
+    address: Joi.object({
+        street: Joi.string().required(),
+        suite: Joi.string().required(),
+        city: Joi.string().required(),
+        zipcode: Joi.string().required(),
+        geo: Joi.object({
+            lat: Joi.string().required(),
+            lng: Joi.string().required()
+        })
+    }),
+    phone: Joi.string().required(),
+    website: Joi.string().required(),
+    company: Joi.object({
+        name: Joi.string().required(),
+        catchPhrase: Joi.string().required(),
+        bs: Joi.string().required()
+    })
+});
+
 
 export default function Users() {
     const [users, setUsers] = useState([]);
     const [aboutController, setAboutController] = useState(new AbortController());
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+
+    const validateUser = useCallback((user) => {
+        const result = Joi.validate(user, usersSchema);
+        return !result.error;
+    }, []);
 
     const cancel = useCallback(() => {
         aboutController.abort();
@@ -27,6 +58,12 @@ export default function Users() {
                 return Promise.reject(new Error('Failed to load'));
             })
             .then(newUsers => {
+                if (!newUsers.every(validateUser)) {
+                    throw new Error('Invalid data');
+                }
+
+                console.log(newUsers);
+                console.log(process.env.REACT_APP_SOME_VALUE);
                 setUsers(newUsers);
             })
             .catch(error => {
@@ -43,11 +80,16 @@ export default function Users() {
         <Fragment>
             {loading && <p>Loading...</p>}
             {error && <p>{error.message}</p>}
-            <ul>
+            <ol>
                 {users.map(user => (
-                    <li key={user.id}>{user.name}</li>
+                    <li key={user.id}>
+                        <h3>{user.name}</h3>
+                        <p>{user.email}</p>
+                        <p>{user.phone}</p>
+                        <p>{user.website}</p>
+                    </li>
                 ))}
-            </ul>
+            </ol>
             <button onClick={cancel}>Cancel</button>
         </Fragment>
     )
